308452a918f6a3754c5d743574be2bce84e428bd
MrChocolate <mrchocolatepr@gmail.com>
2024-05-09 21:09:18 -0400

    agregando base de proyecto con SpringBoot-Maven

---
+++ src/test/java/com/example/coworkingprojectback/CoworkingProjectBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coworkingprojectback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoworkingProjectBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
