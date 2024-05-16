c268c57b0f3dffa0d3425a710d05fdf8c3048463
arielibarbalz <ibarbalz@gmail.com>
2024-05-16 12:19:09 -0300

    Funcionalidades de back y conexion de base de datos

--- src/test/java/com/example/coworkingprojectback/CoworkingProjectBackApplicationTests.java
+++ src/test/java/com/example/coworkingprojectback/CoworkingProjectBackApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.coworkingprojectback;
 
+import com.example.coworkingprojectback.entity.Sala;
+import com.example.coworkingprojectback.service.SalaService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class CoworkingProjectBackApplicationTests {
 
+	@Autowired
+	private SalaService salaService;
+
 	@Test
 	void contextLoads() {
 	}
-
 }
